bfa4f17760014fb539c4f01290b1a003e92b25e1
CenariusYZ <409004007@qq.com>
2021-04-28 21:01:45 +0800

    lab4 页表实验

--- os/src/main.rs
+++ os/src/main.rs
@@ -14,6 +14,8 @@ mod memory;
 
 extern crate alloc;
 
+use crate::sbi::*;
+
 global_asm!(include_str!("entry.asm"));
 
 
@@ -28,18 +30,13 @@ pub extern "C" fn rust_main() -> ! {
     interrupt::init();
     memory::init();
 
-    // 物理页分配
-    for _ in 0..2 {
-        let frame_0 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
-            Result::Ok(frame_tracker) => frame_tracker,
-            Result::Err(err) => panic!("{}", err)
-        };
-        let frame_1 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
-            Result::Ok(frame_tracker) => frame_tracker,
-            Result::Err(err) => panic!("{}", err)
-        };
-        println!("{} and {}", frame_0.address(), frame_1.address());
-    }
+    println!("Liao Haoshen");
 
-   sbi::shutdown();
+    let remap = memory::mapping::MemorySet::new_kernel().unwrap();
+    remap.activate();
+
+    println!("kernel remapped");
+
+    sbi::shutdown();
 }
+
--- os/src/memory/frame/allocator.rs
+++ os/src/memory/frame/allocator.rs
@@ -1,8 +1,12 @@
-use lazy_static::*;
+//! 提供帧分配器 [`FRAME_ALLOCATOR`](FrameAllocator)
+//!
+//! 返回的 [`FrameTracker`] 类型代表一个帧，它在被 drop 时会自动将空间补回分配器中。
+
 use super::*;
-use spin::Mutex;
-use algorithm::*;
 use crate::memory::*;
+use algorithm::*;
+use lazy_static::*;
+use spin::Mutex;
 
 lazy_static! {
     /// 帧分配器
